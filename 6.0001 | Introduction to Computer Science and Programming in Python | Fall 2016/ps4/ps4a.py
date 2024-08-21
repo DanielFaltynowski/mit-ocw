600f8b8d7b7cdf07e6fab7cb6a65d03fc9e9fca0
Daniel Faltynowski <danielfaltynowski@MacBook-Pro-Daniel.local>
2024-08-21 21:23:22 +0200

    6.0001 Problem Set 4: Part A

--- 6.0001 | Introduction to Computer Science and Programming in Python | Fall 2016/ps4/ps4a.py
+++ 6.0001 | Introduction to Computer Science and Programming in Python | Fall 2016/ps4/ps4a.py
@@ -41,18 +41,12 @@ def get_permutations(sequence):
 if __name__ == '__main__':
 #    #EXAMPLE
    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
    print('Actual Output:', get_permutations(example_input))
 
    example_input = 'bust'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
    print('Actual Output:', get_permutations(example_input))
 
    example_input = 'daniel'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
    print('Actual Output:', get_permutations(example_input))
 
 #    # Put three example test cases here (for your sanity, limit your inputs
