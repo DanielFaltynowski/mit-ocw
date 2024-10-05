5cb559c80cbb174d6a7419fb892b2f2f0de4e413
Daniel Faltynowski <danielfaltynowski@MacBook-Pro-Daniel.local>
2024-10-05 19:11:51 +0200

    6.0001 Problem Set 5: Problem 1

--- 6.0001 | Introduction to Computer Science and Programming in Python | Fall 2016/ps5/ps5.py
+++ 6.0001 | Introduction to Computer Science and Programming in Python | Fall 2016/ps5/ps5.py
@@ -56,6 +56,28 @@ def process(url):
 
 # TODO: NewsStory
 
+class NewsStory(object):
+    def __init__(self, guid, title, description, link, pubdate):
+        self.__guid = guid
+        self.__title = title
+        self.__description = description
+        self.__link = link
+        self.__pubdate = pubdate
+
+    def get_guid(self):
+        return self.__guid
+    
+    def get_title(self):
+        return self.__title
+    
+    def get_description(self):
+        return self.__description
+    
+    def get_link(self):
+        return self.__link
+    
+    def get_pubdate(self):
+        return self.__pubdate
 
 #======================
 # Triggers
